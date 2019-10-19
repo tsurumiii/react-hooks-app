3fc79675fa5f3ce824f6990d513631235fc9d21c
tsurumiii <tsurumi0302@gmail.com>
2019-10-19 18:22:54 +0900

    update logic front separation [src/App.js]

--- src/App.js
+++ src/App.js
@@ -3,18 +3,29 @@ import logo from './logo.svg';
 import './styles/App.css';
 import './styles/tailwind.css'
 
-function App() {
-  const [count, setCount] = useState(0);
-  const [name, setName] = useState('Yuki')
+function useUserHook() {
+  const [name, setName] = useState('tsurumi')
 
+  useEffect(() => {
+    console.log('useUserHook', name)
+  }, [name])
+
+  return [name, setName];
+}
+
+function useCountHook() {
+  const [count, setCount] = useState(0);
   useEffect(() => {
     console.log(count);
     document.title = `You clicked ${count} times`;
   }, [count])
 
-  useEffect(() => {
-    console.log(name)
-  }, [name])
+  return [count, setCount];
+}
+
+function App() {
+  const [count, setCount] = useCountHook()
+  const [user, setUser] = useUserHook()
 
   return (
     <div className="App">
@@ -33,15 +44,17 @@ function App() {
         </a>
         <div className="bg-gray-100 text-gray-600 w-full">
           <p>You clicked {count} times</p>
-          <button onClick={() => setCount(count + 1)} className="border">
-            Click me
-          </button>
-          <button onClick={() => setCount(count - 1)} className="border">
-            Click minus
-          </button>
-          <button onClick={() => setName('Yuki Tsurumi')} className="border">
-            cleck change name {name}
-          </button>
+          <div className="flex justify-around mb-2">
+            <button onClick={() => setCount(count + 1)} className="block border px-2 px-2 rounded bg-blue-500 hover:bg-blue-400 text-white">
+              Click me
+            </button>
+            <button onClick={() => setCount(count - 1)} className="block px-2 border rounded bg-teal-600 hover:bg-teal-400 text-white">
+              Click minus
+            </button>
+            <button onClick={() => setUser('Yuki Tsurumi')} className="block px-2 rounded bg-red-500 hover:bg-red-400 text-black hover:text-white border">
+              cleck change name {user}
+            </button>
+          </div>
         </div>
       </header>
     </div >
