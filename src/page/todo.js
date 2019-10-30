0ce05e758edcfd96966ab2413a20bf2bfca7d8b8
tsurumiii <tsurumi0302@gmail.com>
2019-10-30 18:57:43 +0900

    add page to todo page [src/page/todo.js]

---
+++ src/page/todo.js
@@ -0,0 +1,70 @@
+import React, { useState, useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { useHistory } from 'react-router-dom'
+
+function useInputHook() {
+  const [input, setInput] = useState('')
+  return [input, setInput];
+}
+
+function TodoPage() {
+  const [input, setInput] = useInputHook()
+
+  const history = useHistory()
+  const dispatch = useDispatch()
+
+  const store = useSelector(state => state.Topreducer)
+
+
+  const handleInputChange = (event) => {
+    console.log(event.target.value)
+    setInput(event.target.value)
+  }
+
+  const addTodoFunction = () => {
+    if (input.length != 0) {
+      console.log(input)
+      dispatch({
+        type: 'ADDTODO', todo: {
+          todo: input,
+          done: false,
+        }
+      })
+      setInput('')
+      console.log(store.todos)
+    } else {
+      alert('フォームになんもないぞ')
+    }
+
+  }
+
+  return (
+    <div className='h-screen w-screen bg-gray-900 text-center'>
+
+      <h1 className=" text-6xl text-white">TODO LIST</h1>
+      <div className="flex justify-center">
+        <input
+          className="bg-gray-200 hover:bg-white hover:border-gray-300 focus:outline-none focus:bg-white focus:shadow-outline focus:border-gray-300 appearance-none border border-transparent rounded py-2 px-4 text-gray-700 leading-tight"
+          type='text'
+          value={input}
+          onChange={handleInputChange}
+        />
+        <button
+          onClick={() => addTodoFunction()}
+          className="ml-4 flex-shrink-0 bg-teal-500 hover:bg-teal-600 focus:outline-none focus:shadow-outline text-white font-bold py-2 px-4 rounded"
+        >追加</button>
+        <button
+          onClick={() => history.goBack()}
+          className=" ml-4 flex-shrink-0 bg-teal-500 hover:bg-teal-600 focus:outline-none focus:shadow-outline text-white font-bold py-2 px-4 rounded"
+        >戻る</button>
+      </div>
+      <div>
+        {store.todos.map((todo, index) => {
+          return <div key={index} className="text-white">{todo.todo}</div>
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default TodoPage;
